c394dca6376ef58e4db0f5c5266bade22b92d667
Alberto Martinez <martini73.aml@gmail.com>
2020-02-27 17:52:31 +0100

    Finished

---
+++ ExamenAlbertoMartinezCambioDivisas/Services/Log/LogFichero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ExamenAlbertoMartinezCambioDivisas.Services.Log
+{
+    //Escibir en TXt
+    public class LogFichero : ILog
+    {
+        private readonly string path;
+        public LogFichero()
+        {
+
+            this.path = AppDomain.CurrentDomain.BaseDirectory + "LogFiles";
+            Directory.CreateDirectory(path);
+
+        }
+        public void EscribirLog(string message)
+        {
+            var fechaHoy = DateTime.Now;
+
+            using (var sw = new StreamWriter(this.path + "/logfile.txt", true))
+            {
+                sw.WriteLine($"[{fechaHoy}] {message}");
+            }
+        }
+    }
+}
